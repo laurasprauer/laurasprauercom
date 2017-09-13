316a207c407812a6751fca2ce92c9b3a0f7fd069
Laura Sprauer <lspraue@gmail.com>
2017-09-12 21:30:13 -0400

    Added letter component

--- src/components/Main/components/Home/index.js
+++ src/components/Main/components/Home/index.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+//import components
+import Letters from './components/Letters';
+
 class Home extends Component {
   render() {
     return (
       <div className="homeContainer">
         <div className="homeBlock">
           <div className="svg-row1">
-            <div className="svg1">
-              <svg width="109" height="112" viewBox="0 0 109 112" xmlns="http://www.w3.org/2000/svg">
-                <path d="M47.535 96.228L64.878 0h23.976l20.088 111.456H92.256l-4.698-26.73H65.85l-4.536 26.73H.564V0H17.25v96.228h30.285zM77.19 18.144h-.972l-8.586 51.84h18.144l-8.586-51.84z" fill="#FFF" fill-rule="evenodd"/>
-              </svg>
-            </div>
+            <Letters
+              letter="LA"
+            />
+            <Letters
+              letter="U"
+            />
+            <Letters
+              letter="R"
+            />
+            <Letters
+              letter="A"
+            />
+          </div>
+          <div className="svg-row2">
+            <Letters
+              letter="S"
+            />
+            <Letters
+              letter="P"
+            />
+            <Letters
+              letter="R"
+            />
+            <Letters
+              letter="A"
+            />
+            <Letters
+              letter="U"
+            />
+            <Letters
+              letter="ER"
+            />
           </div>
         </div>
       </div>
