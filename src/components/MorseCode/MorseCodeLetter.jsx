52ebad616d5ca75d464e9a0a3ddc312b146c9771
Laura Sprauer <lspraue@gmail.com>
2018-03-03 16:19:36 -0500

    random start position

--- src/components/MorseCode/MorseCodeLetter.jsx
+++ src/components/MorseCode/MorseCodeLetter.jsx
@@ -20,7 +20,7 @@ export default class MorseCodeLetter extends React.Component {
 
     this.state = {
       direction,
-      animationStartPos: Math.floor(Math.random() * this.props.windowWidth) + 1,
+      animationPos: `${Math.floor(Math.random() * 100) + 1}%`,
     };
   }
 
@@ -28,10 +28,15 @@ export default class MorseCodeLetter extends React.Component {
     // define the inline styles
     const letterStyle = {
       top: `${(this.props.windowHeight / this.props.total) * this.props.order}px`,
+      left: `${this.state.animationPos}`,
     };
 
     // define the size of the dot and dash components, subtract 10 for padding
     const size = (this.props.windowHeight / this.props.total) - 10;
+
+    // also get the dot and dash width
+    const dotWidth = this.props.windowHeight / this.props.total;
+    const dashWidth = (size / 0.554545) + 10;
 
     // define what letter will be displayed
     let displayLetter = null;
@@ -39,64 +44,80 @@ export default class MorseCodeLetter extends React.Component {
     switch (this.props.letter) {
       case 'A':
         displayLetter = (
-          <span>
+          <div
+            style={{ width: `${dotWidth + (dashWidth)}px` }}
+          >
             <Dot size={size} />
             <Dash size={size} />
-          </span>
+          </div>
         );
         break;
       case 'E':
         displayLetter = (
-          <span>
+          <div
+            style={{ width: `${dotWidth}px` }}
+          >
             <Dot size={size} />
-          </span>
+          </div>
         );
         break;
       case 'L':
         displayLetter = (
-          <span>
+          <div
+            style={{ width: `${(dotWidth * 3) + (dashWidth)}px` }}
+          >
             <Dot size={size} />
             <Dash size={size} />
             <Dot size={size} />
             <Dot size={size} />
-          </span>
+          </div>
         );
         break;
       case 'P':
         displayLetter = (
-          <span>
+          <div
+            style={{ width: `${(dotWidth * 2) + (dashWidth * 2)}px` }}
+          >
             <Dot size={size} />
             <Dash size={size} />
             <Dash size={size} />
             <Dot size={size} />
-          </span>
+          </div>
         );
         break;
       case 'R':
         displayLetter = (
-          <span>
+          <div
+            style={{ width: `${(dotWidth * 2) + dashWidth}px` }}
+          >
             <Dot size={size} />
             <Dash size={size} />
             <Dot size={size} />
-          </span>
+          </div>
         );
         break;
       case 'S':
         displayLetter = (
-          <span>
+          <div
+            id="hey"
+            onClick={this.animate}
+            style={{ width: `${(dotWidth * 3)}px` }}
+          >
             <Dot size={size} />
             <Dot size={size} />
             <Dot size={size} />
-          </span>
+          </div>
         );
         break;
       case 'U':
         displayLetter = (
-          <span>
+          <div
+            style={{ width: `${(dotWidth * 2) + dashWidth}px` }}
+          >
             <Dot size={size} />
             <Dot size={size} />
             <Dash size={size} />
-          </span>
+          </div>
         );
         break;
       default:
