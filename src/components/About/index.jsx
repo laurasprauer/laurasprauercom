90947cc7e7135b09eb6bea035effec2c42597e0d
Laura Sprauer <lspraue@gmail.com>
2018-03-16 21:29:35 -0400

    about page tweaks

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 function About() {
+  const date = new Date();
+  const exp = date.getFullYear() - 2014;
   return (
     <div className={styles.container} >
       <div className={styles.wrapper} >
         <div className={styles.content} >
-          <h1>Who Dis?</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus mi sed lacus iaculis, vitae tincidunt quam volutpat. Sed nibh quam, luctus et ornare vitae, bibendum sed nunc. Morbi mattis non neque ut aliquet.</p>
+          <h1>Hello</h1>
+          <p>My name is Laura Sprauer, and I&#39;ve been creatively developing for the past {exp} years. Starting off my career as an interactive designer before fully transitioning to a front end developer has given me a unique perspective - and I love it!</p>
+          <p>I&#39;m also a nerd and pet lover who calls Cincinnati home. Interested in connecting? Let&#39;s talk!</p>
         </div>
         <div className={styles.blackBox} />
         <div className={styles.whiteBox} />
