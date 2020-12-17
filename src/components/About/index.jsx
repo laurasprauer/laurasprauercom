965241873f45ffc40e02f4258154857c07acdebb
Laura Sprauer <lsprauer@quotient.com>
2020-12-17 11:37:47 -0500

    finish updated about me text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -14,7 +14,7 @@ function About() {
         <div className={styles.content}>
           <div className={styles.aboutHeader}>
             <img src={imageSrc} alt="Laura Sprauer" />
-            <h1>Hey&nbsp;&nbsp;There</h1>
+            <div className={styles.heyThere}><h1>Hey&nbsp;&nbsp;There</h1></div>
           </div>
           <p>
             My name is Laura Sprauer, and I&#39;ve been developing on the web for the past
@@ -22,8 +22,17 @@ function About() {
             years. Starting off my career as an interactive designer before fully transitioning to a front end engineer has given me a unique perspective - and I love it!
           </p>
           <p>
-            Interested in connecting? &nbsp;
+            During the day I build slick user interfaces for a more beautiful and easy to use web - during the evenings you can find me walking my dog, spending quality time with friends and family, and deep diving into all things arts and crafts.
+          </p>
+          <p>
+            Interested in connecting? I&#39;m available for new opportunities, speaking engagments, or networking over coffee. &nbsp;
             <Link to="/contact">Let&#39;s Talk!</Link>
+          </p>
+          <p>
+            Want to learn more about my professional experience? &nbsp;
+            <Link to="/contact">View My Resume</Link> &nbsp;
+            or connect with me on &nbsp;
+            <a href="https://www.linkedin.com/in/laurasprauer/" target="_blank" rel="noopener noreferrer">LinkedIn</a>.
           </p>
         </div>
         <div className={styles.blackBox} />
