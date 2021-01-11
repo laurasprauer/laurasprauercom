009fbbc8a9dedc672b89b7f9cc03325916feb95b
Laura Sprauer <lsprauer@quotient.com>
2021-01-11 11:51:37 -0500

    all info is done

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -89,13 +89,46 @@ export default class Resume extends React.Component {
                 jobTitle="Education"
                 companyName="Bowling Green State University"
                 companyLink="https://www.bgsu.edu/"
-                jobDescription="Bachelor of Fine Arts (BFA), Digital Art <br> Minor in Computer Science"
+                jobDescription="Bachelor of Fine Arts (BFA), Digital Art <br> Minor in Computer Science <br><br>Activities and Societies: <a href='https://bgsu.siggraph.org/' target='_blank' rel='noopener noreferrer'>Computer Art Club: SIGGRAPH Chapter</a>, <a href='https://www.bgsu.edu/learning-and-theme-communities/arts-village.html' target='_blank' rel='noopener noreferrer'>Arts Village</a>, <a href='https://www.bgsu.edu/news/2014/06/murals-brighten-toledo-communities.html' target='_blank' rel='noopener noreferrer'>Toledo Old South End Mural Project</a>, <a href='https://www.bgsu.edu/news/2018/09/last-in-a-series-trip-to-navajo-nation-changes-students-lives.html' target='_blank' rel='noopener noreferrer'>Navajo Nation Cultural Immersion Experience</a>"
                 start="2010"
                 end="2013"
               />
             </div>
             <div className={styles.timelineLine} style={{ height: this.state.timelineHeight }}>
               <div className={styles.line}/>
+            </div>
+          </div>
+
+          <div className={styles.skills}>
+            <div className={styles.skillsTitle}>Known Technologies</div>
+            <div className={styles.skillsDescription}>
+              <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a>,&ensp;
+              <a href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">Redux</a>,&ensp;
+              <a href="https://angular.io/" target="_blank" rel="noopener noreferrer">Angular</a>,&ensp;
+              <a href="https://emberjs.com/" target="_blank" rel="noopener noreferrer">Ember</a>,&ensp;
+              Javascript,&ensp;CSS/SCSS,&ensp;HTML,&ensp;SVG,&ensp;Web Animation,&ensp;
+              <a href="https://webpack.js.org/" target="_blank" rel="noopener noreferrer">Webpack</a>,&ensp;
+              JSON,&ensp;
+              <a href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer">Node.js</a>,&ensp;
+              GIT,&ensp;
+              <a href="https://github.com/" target="_blank" rel="noopener noreferrer">Github</a>,&ensp;
+              Linters,&ensp;
+              <a href="https://jquery.com/" target="_blank" rel="noopener noreferrer">JQuery</a>,&ensp;
+              <a href="https://wordpress.com/" target="_blank" rel="noopener noreferrer">Wordpress</a>,&ensp;
+              <a href="https://firebase.google.com/" target="_blank" rel="noopener noreferrer">Firebase</a>,&ensp;
+              <a href="https://aws.amazon.com/" target="_blank" rel="noopener noreferrer">AWS</a>,&ensp;
+              <a href="https://www.heroku.com/" target="_blank" rel="noopener noreferrer">Heroku</a>,&ensp;
+              <a href="https://www.serverless.com/" target="_blank" rel="noopener noreferrer">Serverless</a>,&ensp;
+              <a href="https://www.atlassian.com/software/jira" target="_blank" rel="noopener noreferrer">JIRA</a>,&ensp;
+              Kanban,&ensp;Agile Methodology,&ensp;
+              <a href="https://www.php.net/" target="_blank" rel="noopener noreferrer">PHP</a>,&ensp;
+              <a href="https://coldfusion.adobe.com/" target="_blank" rel="noopener noreferrer">ColdFusion</a>,&ensp;
+              <a href="https://www.murasoftware.com/" target="_blank" rel="noopener noreferrer">Mura CMS</a>,&ensp;
+              Email Development,&ensp;
+              <a href="https://www.sketch.com/" target="_blank" rel="noopener noreferrer">Sketch</a>,&ensp;
+              <a href="https://www.adobe.com/products/illustrator.html" target="_blank" rel="noopener noreferrer">Adobe Illustrator</a>,&ensp;
+              <a href="https://www.adobe.com/products/photoshop.html" target="_blank" rel="noopener noreferrer">Adobe Photoshop</a>,&ensp;
+              <a href="https://www.invisionapp.com/" target="_blank" rel="noopener noreferrer">Invision</a>
             </div>
           </div>
         </div>
