a7986874fa78a980d187226fbde0c4407420fa6d
Laura Sprauer <lspraue@gmail.com>
2021-01-15 14:41:42 -0500

    Fixing broken builds (#7)
    * round one of package updates
    
    * webpack updated
    
    * updating other dependencies
    
    * fix dist production build in webpack
    
    * final updates before starting on the fun part
    
    * about page revamp work in progress
    
    * finish updated about me text
    
    * updating more dependeciies
    
    * continuing on resume looks freaking great
    
    * dark mode and content added to resume
    
    * all info is done
    
    * downloadpdf button added
    
    * resume pdf added
    
    * I broke the build oops
    
    * another fix
    
    Co-authored-by: Laura Sprauer <lsprauer@quotient.com>

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     publicPath: '/',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   devServer: {
     host: 'localhost',
