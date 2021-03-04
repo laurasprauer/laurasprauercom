57f3a843cc40079c8d6d28114325e3367b7afae3
Laura Sprauer <lsprauer@quotient.com>
2021-03-04 15:39:39 -0500

    adding check to contact form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -16,13 +16,43 @@ AWS.config.update({
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+const MAX_CHARACTER_COUNT = 500;
+
 class Contact extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       error: null,
+      characterCount: 0,
+      name: '',
+      email: '',
+      message: '',
     };
+  }
+
+  checkCharacterCount = () => {
+    setTimeout(() => {
+      // don't hate me cuz I'm beautiful
+      const message = document.getElementById('message').value;
+
+      this.setState({
+        message,
+        characterCount: message.length,
+      });
+    });
+  }
+
+  saveInput = (e) => {
+    if (e.target.id === 'name') {
+      this.setState({
+        name: e.target.value,
+      });
+    } else if (e.target.id === 'email') {
+      this.setState({
+        email: e.target.value,
+      });
+    }
   }
 
   handleSuccess = () => {
@@ -38,9 +68,10 @@ class Contact extends React.Component {
 
   recordMessage = (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+
+    const { name } = this.state;
+    const { email } = this.state;
+    const { message } = this.state;
 
     // check to make sure all the correct info was provided
     if (name === '') {
@@ -53,11 +84,15 @@ class Contact extends React.Component {
       });
     } else if (this.validateEmail(email) === false) {
       this.setState({
-        error: 'Oops! This is not a valid email address',
+        error: 'Oops! Your email address is not valid',
       });
     } else if (message === '') {
       this.setState({
         error: 'Oops! You forgot to add a message.',
+      });
+    } else if (message.length > MAX_CHARACTER_COUNT) {
+      this.setState({
+        error: 'Oops! Your message is too long. Please keep it under 500 characters',
       });
     } else if (this.validateEmail(email) === true) {
       // let's get the date
@@ -96,16 +131,34 @@ class Contact extends React.Component {
       displayError = <p className={styles.error}>{this.state.error}</p>;
     }
 
+    // display the character count
+    let displayCharacterCount = null;
+    if (this.state.characterCount <= MAX_CHARACTER_COUNT && this.state.characterCount > MAX_CHARACTER_COUNT * 0.9) {
+      displayCharacterCount = <p className={styles.characterCountWarning}>{this.state.characterCount} / {MAX_CHARACTER_COUNT}</p>;
+    } else if (this.state.characterCount <= MAX_CHARACTER_COUNT) {
+      displayCharacterCount = <p className={styles.characterCount}>{this.state.characterCount} / {MAX_CHARACTER_COUNT}</p>;
+    } else if (this.state.characterCount > MAX_CHARACTER_COUNT) {
+      displayCharacterCount = <p className={styles.characterCountError}>{this.state.characterCount} / {MAX_CHARACTER_COUNT}</p>;
+    }
+
+    // should the button be disabled?
+    let btn = <input type="submit" value="Send Message" className={styles.btn} onClick={this.recordMessage}/>;
+
+    if (!(this.state.name && this.state.email && this.state.message && this.state.characterCount <= MAX_CHARACTER_COUNT)) {
+      btn = <input type="submit" value="Send Message" className={styles.disableBtn} onClick={this.recordMessage}/>;
+    }
+
     return (
       <div className={styles.container}>
         <div className={styles.wrapper}>
           <h1>Let&#39;s Talk!</h1>
-          <form>
-            <input type="text" id="name" name="name" placeholder="Your Name..." />
-            <input type="text" id="email" name="email" placeholder="Your Email..." />
-            <textarea id="message" name="message" placeholder="Your Message..." />
+          <form className={styles.formContainer} onSubmit={(e) => { e.preventDefault(); }}>
+            <input type="text" id="name" name="name" placeholder="Your Name..." onKeyDown={this.saveInput} onBlur={this.saveInput} tabIndex="0" />
+            <input type="text" id="email" name="email" placeholder="Your Email..." onKeyDown={this.saveInput} onBlur={this.saveInput} tabIndex="0" />
+            <textarea id="message" name="message" placeholder="Your Message..." onKeyDown={this.checkCharacterCount} onBlur={this.checkCharacterCount} tabIndex="0" />
+            {displayCharacterCount}
             {displayError}
-            <input type="submit" value="Send Message" className={styles.btn} onClick={this.recordMessage} />
+            {btn}
           </form>
         </div>
       </div>
