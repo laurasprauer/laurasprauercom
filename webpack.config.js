01d994ba4a4194f30738f6d1a0b5bebcd8a38e0d
Laura Sprauer <lspraue@gmail.com>
2021-10-05 15:57:28 -0400

    another test

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 // Constant with our paths
 const paths = {
@@ -13,97 +14,104 @@ const paths = {
 };
 
 // Webpack configuration
-module.exports = {
-  mode: 'production',
-  performance: { hints: false },
-  entry: path.join(paths.JS, 'website.jsx'),
-  output: {
-    filename: '[name].bundle.js',
-    publicPath: '/',
-    path: path.resolve(__dirname, 'public'),
-  },
-  devServer: {
-    host: 'localhost',
-    port: 3000,
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, 'index.html'),
-    }),
-    new MiniCssExtractPlugin({ filename: 'style.css' }),
-    new StyleLintPlugin({
-      configFile: '.stylelintrc.json',
-      context: 'src',
-      syntax: 'scss',
-      files: '**/*.scss',
-    }),
-    new Dotenv(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              import: false,
-              modules: true,
-            },
-          },
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-        exclude: /\.module\.css$/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              import: false,
-              modules: true,
-              camelCase: true,
-              localIdentName: '[name]__[local]__[hash:base64:5]',
-            },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        enforce: 'pre',
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        // options: {
-        //   cache: './.eslint-loader-cache',
-        // }
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
+module.exports = (env) => (
+  {
+    mode: 'production',
+    performance: { hints: false },
+    entry: path.join(paths.JS, 'website.jsx'),
+    output: {
+      filename: '[name].bundle.js',
+      publicPath: '/',
+      path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+      host: 'localhost',
+      port: 3000,
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(paths.SRC, 'index.html'),
+      }),
+      new MiniCssExtractPlugin({ filename: 'style.css' }),
+      new StyleLintPlugin({
+        configFile: '.stylelintrc.json',
+        context: 'src',
+        syntax: 'scss',
+        files: '**/*.scss',
+      }),
+      ...(env.development ? [new webpack.DefinePlugin({
+        'process.env': {
+          REACT_APP_ENV: JSON.stringify(process.env.REACT_APP_ENV),
+          REACT_APP_MIXPANEL_TOKEN: JSON.stringify(process.env.REACT_APP_MIXPANEL_TOKEN),
+        },
+      })] : [new Dotenv()]),
     ],
-  },
-  // Enable importing JS files without specifying their's extenstion
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-};
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                import: false,
+                modules: true,
+              },
+            },
+          ],
+          include: /\.module\.css$/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
+          exclude: /\.module\.css$/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                import: false,
+                modules: true,
+                camelCase: true,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+            },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+        {
+          enforce: 'pre',
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          // options: {
+          //   cache: './.eslint-loader-cache',
+          // }
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    // Enable importing JS files without specifying their's extenstion
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  }
+);
