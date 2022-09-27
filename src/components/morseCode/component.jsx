6b6198ebf9cafa9250302a37902b0fb682b2a505
Laura Sprauer <lspraue@gmail.com>
2022-09-27 09:55:31 -0400

    trying to fix weird mobile issue with annimation

--- src/components/morseCode/component.jsx
+++ src/components/morseCode/component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import MorseCodeAnimate from './morseCodeAnimate';
@@ -6,42 +6,58 @@ import MorseCodeAnimate from './morseCodeAnimate';
 import * as styles from './styles.module.scss';
 
 export const MorseCode = ({ darkmode, theme }) => {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
+  const [windowSize, _setWindowSize] = useState({
+    width: 0,
+    height: 0,
   });
   const [displayAnimation, setDisplayAnimation] = useState(false);
+  const myStateRef = React.useRef(windowSize);
+  const setMyState = (data) => {
+    myStateRef.current = data;
+    _setWindowSize(data);
+  };
 
   useEffect(() => {
     if (window && document) {
       // Handler to call on window resize
       function handleResize() {
         // Set window width/height to state
-        setWindowSize({
+        setMyState({
           width: window.innerWidth,
           height: window.innerHeight,
-          oldWidth: windowSize.width,
-          oldHeight: windowSize.height,
+          oldWidth: myStateRef.current.width,
+          oldHeight: myStateRef.current.height,
         });
       }
       // Add event listener
       window.addEventListener('resize', handleResize);
       // Call handler right away so state gets updated with initial window size
       handleResize();
+      setDisplayAnimation(true);
       // Remove event listener on cleanup
       return () => window.removeEventListener('resize', handleResize);
     }
   }, []);
 
   useEffect(() => {
+    const heightUpdated =
+      windowSize.height &&
+      getDifference(windowSize.height, windowSize.oldHeight) > 50;
+    const widthUpdated =
+      windowSize.width && windowSize.width !== windowSize.oldWidth;
+
     // force animation reload on window resize
-    if (windowSize.width && windowSize.width !== windowSize.oldWidth) {
+    if (heightUpdated || widthUpdated) {
       setDisplayAnimation(false);
       setTimeout(() => {
         setDisplayAnimation(true);
       }, 100);
     }
   }, [windowSize]);
+
+  const getDifference = (a, b) => {
+    return Math.abs(a - b);
+  };
 
   const fullName = [...'LAURASPRAUER'];
 
